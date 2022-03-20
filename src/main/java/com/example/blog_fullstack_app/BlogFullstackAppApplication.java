2efdf609b1ce1fc1f6233a4c1e69d365d6b9cf06
leadmombo <mombomombo@et.esiea.fr>
2022-03-20 05:36:49 +0100

    Setup project layout

---
+++ src/main/java/com/example/blog_fullstack_app/BlogFullstackAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.blog_fullstack_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogFullstackAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogFullstackAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/blog_fullstack_app/BlogFullstackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blog_fullstack_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogFullstackAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
